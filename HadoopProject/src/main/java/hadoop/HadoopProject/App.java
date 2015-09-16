74bf6efc44b67f92cd50e0e724e3f79e843d0a67
pawan <pawan@pawan-Latitude-E7450>
2015-09-16 11:29:41 +0100

    Testing Git

---
+++ HadoopProject/src/main/java/hadoop/HadoopProject/App.java
@@ -0,0 +1,13 @@
+package hadoop.HadoopProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
